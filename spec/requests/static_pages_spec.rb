0961f5164e0732dbeea234094068adaf32be9763
Cspearing <Chris@spearing.me>
2013-03-17 20:03:37 +0000

    refactored r-spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,55 +2,54 @@ require 'spec_helper'
 
 describe "Static Pages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
 
   describe "Home Page" do
-	it "should have sthe content 'Sample App'" do
-	 visit '/static_pages/home'
-	 page.should have_content('Sample App')    
-    	end
+	before { visit home_path }
+	
+	it { should have_content('Sample App')}
 
-	it "should have the right title" do
-	 visit "/static_pages/home"
-	 page.should have_selector('title',
-		:text => "#{base_title}")
-	end 
-	it "should not have a custom title" do
-	 visit "/static_pages/home"
-	 page.should_not have_selector('title', :text => '| Home')
-	end
+	it { should have_selector('title', :text => "#{base_title}")}
+	
+	it { should_not have_selector('title', :text => '| Home')}
    end
 
   describe "Help Page" do
 	it "should have the content 'Help Page'" do
-		visit '/static_pages/help'
+		visit help_path
 		page.should have_content('Help Page')
 	end
 
 	it "should have the right title" do
-	 visit "/static_pages/help"
+		visit help_path
 	 page.should have_selector('title',
 		:text => "#{base_title} | Help")
 	end
   end
   describe "About page" do
 	it "should have the right title" do
-	  visit "/static_pages/about"
+	  visit about_path
 	  page.should have_selector('title',
 		:text => "#{base_title} | About")
 	end
 	it "should have the content 'About Us'" do
-		visit '/static_pages/about'
+	  visit about_path
 		page.should have_content('About Us')
 	end
   end
   describe "Contact page" do
+	it "should have h1 'Contact'" do
+		visit contact_path
+		page.should have_selector('h1', text: 'Contact')
+	end
+
 	it "should have the right title" do
-	  visit "/static_pages/contact"
+		visit contact_path
 	  page.should have_selector('title',
 		:text => "#{base_title} | Contact Us")
 	end
 	it "should have the content 'Contact Us'" do
-	  visit "/static_pages/contact"
+		visit contact_path
 	  page.should have_content('Contact Us')
 	end
   end
